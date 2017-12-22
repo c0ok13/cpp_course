a609d0e0b10a5f7da1e01a3696f49e54996fdc5c
c0ok13 <33197240+c0ok13@users.noreply.github.com>
2017-12-22 12:50:44 +0300

    Add files via upload

---
+++ workshop1/worcshop 1.3/CMakeFiles/07.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/07.dir/main.cpp.obj"
+  "07.pdb"
+  "07.exe"
+  "07.exe.manifest"
+  "lib07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
